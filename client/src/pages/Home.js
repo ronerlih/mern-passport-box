fa387893fe5de044df5a68629792e805c631e12b
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-12 14:44:19 -0500

    clean up of home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import API from "../utils/API";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Row } from "../components/Grid";
-import { Table, Tr, Td } from "../components/Table";
-import { ForwardRefInput, FormBtn } from "../components/Form";
+
 
 function Home({ username }) {
 
