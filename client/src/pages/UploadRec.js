d80523a8fab52931ff8dc5198c05d322f6f7770f
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-12 15:10:46 -0500

    upload rec form and display edited

--- client/src/pages/UploadRec.js
+++ client/src/pages/UploadRec.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect, useRef } from "react";
 import DeleteBtn from "../components/DeleteBtn";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
+import Card from "../components/Card";
+import { Input } from "../components/Form";
 import { Container, Col, Row } from "../components/Grid";
 import { Table, Tr, Td } from "../components/Table";
 import { ForwardRefInput, FormBtn } from "../components/Form";
+import { List } from "../components/List";
+import DisplayRecos from "../components/DisplayRecos";
 
 function UploadRec({ username }) {
 
     //setting initial state
-    const [recos, setRecos] = useState([]);
+    const [recoState, setRecoState] = useState({
+        recos: [],
+        prompt: "You Have Not yet Uploaded a Recommendation!"
+    });
     const [formObject, setFormObject] = useState({
         reco_name: "",
         reco_pic: "",
@@ -19,9 +26,6 @@ function UploadRec({ username }) {
         username: ""
     });
 
-    //getting input element ref for focus
-    // const titleInputElRef = useRef();
-    // const picInputElRef = useRef();
 
     useEffect(() => {
         //set user after successful component mount
@@ -36,19 +40,11 @@ function UploadRec({ username }) {
 
         loadMyRecos();
 
-        // focus on titleInputEl if ref exists
-        // titleInputElRef.current.focus()
-        // picInputElRef.current.focus()
     }, [username]);
-
-    //Informs user reco was uploaded
-    // function recoUploaded() {
-    //     alert("uploaded");
-    // }
 
     function loadMyRecos() {
         API.getMyRecos()
-            .then(res => setRecos(res.data))
+            .then(res => setRecoState({ ...recoState, recos: res }))
             .catch(err => console.log(err));
     }
 
@@ -57,55 +53,32 @@ function UploadRec({ username }) {
         const { name, value } = event.target;
         setFormObject({ ...formObject, [name]: value });
     }
-    // function handleInputChange(event) {
-    //     const value = event.target.value;
-    //     setFormObject({ ...formObject, [event.target.name]: value });
-    // }
 
-
-    // When form submitted, use API.saveReco method to save reco data
-    // function handleFormSubmit(event) {
-    //     event.preventDefault();
-    //     if (formObject.description) {
-    //         recoUploaded()
-    //     }
-    // }
 
     // When form submitted, use API.saveReco method to save reco data
     function handleFormSubmit(event) {
         event.preventDefault();
         // if (formObject.body) {
-            API.uploadReco({
-                reco_name: formObject.reco_name,
-                reco_pic: formObject.reco_pic,
-                reco_link: formObject.reco_link,
-                reco_description: formObject.reco_description,
-                reco_keywords: formObject.reco_keywords,
-                username: formObject.username
-            })
-                // .then(recoUploaded)
-                .then(() => setFormObject({
-                    reco_name: "",
-                    reco_pic: "",
-                    reco_link: "",
-                    reco_description: "",
-                    reco_keywords: "",
-                    username: ""
-                }))
-                .catch(err => console.log(err));
+        API.uploadReco({
+            reco_name: formObject.reco_name,
+            reco_pic: formObject.reco_pic,
+            reco_link: formObject.reco_link,
+            reco_description: formObject.reco_description,
+            reco_keywords: formObject.reco_keywords,
+            username: formObject.username
+        })
+            // .then(recoUploaded)
+            .then(() => setFormObject({
+                reco_name: "",
+                reco_pic: "",
+                reco_link: "",
+                reco_description: "",
+                reco_keywords: "",
+                username: ""
+            }))
+            .catch(err => console.log(err));
         // }
     }
-
-
-    //add photo
-    //add link
-    //add description
-    //add keywords
-    //submit button to post 
-
-    //add to recommendation_db
-    //upload associated with user_id
-    //attributes = reco_name, reco_pic, reco_link, reco_description, reco_keywords
 
     return <>
         <Container>
@@ -114,49 +87,54 @@ function UploadRec({ username }) {
                 <Col size='md-12'>
                     <form>
                         <Col size='sm-12'>
-                        
-                                    <input className='form-control' value={formObject.reco_name} onChange={handleInputChange} name='reco_name' placeholder='Title' /> 
-                                    <input className='form-control' value={formObject.reco_description} onChange={handleInputChange} name='reco_description' placeholder='description' /> 
-                                    <input className='form-control' value={formObject.reco_pic} onChange={handleInputChange} name='reco_pic' placeholder='pic' />  
-                                    <input className='form-control' value={formObject.reco_link} onChange={handleInputChange} name='reco_link' placeholder='link' />  
-                                    <input className='form-control' value={formObject.reco_keywords} onChange={handleInputChange} name='reco_keywords' placeholder='keywords' />   
-                           
-                            {/* <ForwardRefInput ref={titleInputElRef} value={formObject.reco_name} onChange={handleInputChange} name='reco_name' placeholder='Title' />
-
-                        <ForwardRefInput ref={picInputElRef} value={formObject.reco_pic} onChange={handleInputChange} name='reco_pic' placeholder='Photo' /> */}
-
-                            {/* <ForwardRefInput ref={titleInputElRef} value={formObject.reco_link} onChange={handleInputChange} name='reco_link' placeholder='Link' />
-
-                        <ForwardRefInput ref={titleInputElRef} value={formObject.reco_description} onChange={handleInputChange} name='reco_description' placeholder='Description' />
-
-                        <ForwardRefInput ref={titleInputElRef} value={formObject.reco_keywords} onChange={handleInputChange} name='reco_keywords' placeholder='Keywords' /> */}
+                            <Card title="Upload a Recommendation">
+                                <form>
+                                    <div className="form-group">
+                                        <Input value={formObject.reco_name} onChange={handleInputChange} name='reco_name' placeholder='Title' />
+                                        <Input className='form-control' value={formObject.reco_description} onChange={handleInputChange} name='reco_description' placeholder='description' />
+                                        <Input className='form-control' value={formObject.reco_pic} onChange={handleInputChange} name='reco_pic' placeholder='pic' />
+                                        <Input className='form-control' value={formObject.reco_link} onChange={handleInputChange} name='reco_link' placeholder='link' />
+                                        <Input className='form-control' value={formObject.reco_keywords} onChange={handleInputChange} name='reco_keywords' placeholder='keywords' />
+                                    </div>
+                                </form>
+                            </Card>
                         </Col>
                         <FormBtn
                             disabled={!formObject.reco_name}
                             onClick={handleFormSubmit}>
                             Upload Recommendation
-					</FormBtn>
+					    </FormBtn>
                     </form>
                 </Col>
             </Row>
 
             <Row>
                 <Col size="md-12">
-                    {recos.length ? (
-                        <Table>
-                            {recos.map(reco => (
-                                <Tr key={reco._id}>
-                                    <Td>
-                                        {reco.reco_name}
-                                    </Td>
-                                </Tr>
-                            ))}
-                        </Table>
-                    ) : (
-                            <h3>Upload a Recommendation</h3>
-                        )}
+                    <Card>
+                        {recoState.recos.length > 0 ? (
+                            <List>
+                                {recoState.recos.map(result => (
+                                    <DisplayRecos
+                                        key={result._id}
+                                        title={result.reco_name}
+                                        pic={result.reco_pic}
+                                        link={result.reco_link}
+                                        description={result.reco_description}
+                                        keywords={result.reco_keywords}
+                                        date={result.reco_date}
+                                        Button={() => (
+                                            <button
+                                                className="btn btn-dark ml-2"
+                                                onClick={() => this.handleRecoSave(result._id)}>Save Recommendation</button>
+                                        )}
 
-
+                                    />
+                                ))}
+                            </List>
+                        ) : (
+                                <h3 className="text-center">{recoState.prompt}</h3>
+                            )}
+                    </Card>
                 </Col>
             </Row>
         </Container>
