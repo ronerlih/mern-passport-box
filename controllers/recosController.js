28f20df837f5a5ec9d8f43cd89976ffee359b403
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-13 11:53:59 -0500

    code to return reco not user in recoController.js

--- controllers/recosController.js
+++ controllers/recosController.js
@@ -19,17 +19,28 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // create: function(req, res) {
+  //    // if no user on the session
+  //    console.log("worked")
+  //    if(!req.user) return res.status(401).end('user isnt authenticated')
+  //   console.log(req.body)
+  //    db.Recos
+  //     .create({username: req.username, reco_name: req.reco_name, reco_pic: req.reco_pic, reco_link: req.reco_link, reco_discription: req.reco_discription, reco_keywords: req.reco_keywords, email: req.user.email})
+  //     .then(({_id}) => db.User.findOneAndUpdate({_id: req.user._id}, { $push: { recos: _id } }, { new: true }))
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
   create: function(req, res) {
-     // if no user on the session
-     console.log("worked")
-     if(!req.user) return res.status(401).end('user isnt authenticated')
-    console.log(req.body)
-     db.Recos
-      .create({username: req.username, reco_name: req.reco_name, reco_pic: req.reco_pic, reco_link: req.reco_link, reco_discription: req.reco_discription, reco_keywords: req.reco_keywords, email: req.user.email})
-      .then(({_id}) => db.User.findOneAndUpdate({_id: req.user._id}, { $push: { recos: _id } }, { new: true }))
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+    // if no user on the session
+    console.log("worked")
+    if(!req.user) return res.status(401).end('user isnt authenticated')
+   console.log(req.body)
+    db.Recos
+     .create({username: req.username, reco_name: req.reco_name, reco_pic: req.reco_pic, reco_link: req.reco_link, reco_discription: req.reco_discription, reco_keywords: req.reco_keywords })
+     .then(dbReco => res.json(dbReco))
+     .then(({_id}) => db.User.findOneAndUpdate({_id: req.user._id}, { $push: { recos: _id } }, { new: true }))
+     .catch(err => res.status(422).json(err));
+ },
 
   // update: function(req, res) {
   //    db.Comment
