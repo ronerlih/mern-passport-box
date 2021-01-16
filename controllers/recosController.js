43034f842a734fedaf010037c051fe039b839f8f
Kentaro Kinoshita <mixmix0321@gmail.com>
2021-01-16 13:28:31 -0500

    added data value for saved recos

--- controllers/recosController.js
+++ controllers/recosController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Recos
-      .find(req.query)
+      .find()
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
